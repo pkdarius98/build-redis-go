ed98756af6c0bb3b2a4506983c2b196ba29081ff
Trinh Duy Hai <pkdarius98@gmail.com>
2026-07-16 18:34:32 +0700

    PING & ECHO

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func handleCommand(args []string) string {
+	cmd := strings.ToUpper(args[0])
+
+	switch cmd {
+	case "PING":
+		if len(args) == 1 {
+			return "+PONG\r\n"
+		}
+
+		return encodeBulkString(args[1])
+	case "ECHO":
+		return encodeBulkString(args[1])
+	}
+
+	return fmt.Sprintf("-ERR unknown command '%s'\r\n", cmd)
+}
+
+func encodeBulkString(s string) string {
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		args := parseArgs(line)
+		response := handleCommand(args)
+		fmt.Print(response)
+	}
+}
+
+func parseArgs(line string) []string {
+	var args []string
+	var current strings.Builder
+	inQuotes := false
+	for _, ch := range line {
+		switch {
+		case ch == '"' && !inQuotes:
+			inQuotes = true
+		case ch == '"':
+			inQuotes = false
+		case ch == ' ' && !inQuotes:
+			if current.Len() > 0 {
+				args = append(args, current.String())
+				current.Reset()
+			}
+		default:
+			current.WriteRune(ch)
+		}
+	}
+	if current.Len() > 0 {
+		args = append(args, current.String())
+	}
+	return args
+}
